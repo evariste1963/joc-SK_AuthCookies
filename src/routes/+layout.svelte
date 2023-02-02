358a3b6ae0f89e5191400289ac136b514ba82b95
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-02 11:24:26 +0000

    ADMIN button hidden when on ADMIN page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { applyAction, enhance } from '$app/forms'
+	import { applyAction, enhance } from '$app/forms';
 	import { invalidateAll } from '$app/navigation';
-	import { page } from '$app/stores'
-	import '../styles/app.css'
+	import { page } from '$app/stores';
+	import '../styles/app.css';
 </script>
 
 <svelte:head>
@@ -11,29 +11,34 @@
 
 <nav>
 	{#if !$page.data.user}
-	<a href="/login">Login</a>
-	<a href="/register">Register</a>
+		<a href="/login">Login</a>
+		<a href="/register">Register</a>
 	{/if}
 
 	{#if $page.data.user}
-	<a href="/admin">Admin</a>
-	
+		<!-- hide ADMIN button when in admin -->
+		{#if !$page.url.pathname.startsWith('/admin')}
+			<a href="/admin">Admin</a>
+		{/if}
+		<form
+			class="logout"
+			action="/logout"
+			method="POST"
+			use:enhance={() => {
+				return async ({ result }) => {
+					//rerun the 'load' function for the page
+					//https://kit.svelte.dev.docs.modules#app-navigation-invalidateall
+					invalidateAll();
 
-	<form class="logout" action="/logout" method="POST" use:enhance={() => {
-		return async ({ result }) => {
-			//rerun the 'load' function for the page
-			//https://kit.svelte.dev.docs.modules#app-navigation-invalidateall
-			invalidateAll()
-	
-			//since we are customizng the default behavior
-			//we don't want to reimplpement what 'use:enhance' does
-			//so we can use 'applyresult' and pass the 'result'
-			await applyAction(result)
-		}
-	}} 
-	>
-	<button type="submit">Log out</button>
-	</form>
+					//since we are customizng the default behavior
+					//we don't want to reimplpement what 'use:enhance' does
+					//so we can use 'applyresult' and pass the 'result'
+					await applyAction(result);
+				};
+			}}
+		>
+			<button type="submit">Log out</button>
+		</form>
 	{/if}
 </nav>
 
