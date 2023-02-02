88109511adf3188670140360d2db7effcaa7855e
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2023-02-02 07:51:51 +0000

    from work done

--- src/routes/(protected)/admin/+page.svelte
+++ src/routes/(protected)/admin/+page.svelte
@@ -1,15 +1,15 @@
 <script>
-    import { page} from '$app/stores'
+	import { page } from '$app/stores';
 </script>
 
 <h1>Admin</h1>
 
 {#if $page.data.user}
-    <p>Welcome {$page.data.user.name}!</p>
+	<p>Welcome {$page.data.user.name}!</p>
 {/if}
 
 {#if $page.data.user.role === 'ADMIN'}
-    <form action="/logout" method="POST">
-        <button type="submit">Log out</button>
-    </form>
+	<form action="/logout" method="POST">
+		<button type="submit">Log out</button>
+	</form>
 {/if}
